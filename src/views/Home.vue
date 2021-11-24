fa15b9cce1308a368f071f515ebbdb09b6a61e05
Davide Cristini <davcri91@gmail.com>
2021-11-24 10:19:31 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </main>
+</template>
